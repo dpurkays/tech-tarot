1b8c95bf9973492202677dca1e3a588b2db54035
Dula Purkaystha <dpurkays@gmail.com>
2025-04-22 17:50:13 -0700

    Load fonts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
+import "./Header.scss";
 function Header() {
     return ( 
-    <header className="site-header">
-        Tech Tarot 🔮
-    </header> );
+        <header className="site-header">
+            <h1 className="title">Tech Tarot 🔮</h1>
+        </header> 
+    );
 }
 
 export default Header;
